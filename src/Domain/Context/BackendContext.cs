7b57befd2f591bdad1eff9363a1cfe69edc4c4e5
João Brambilla <joaobrambilla@outlook.com>
2018-04-26 15:14:20 -0300

    First Commit

---
+++ src/Domain/Context/BackendContext.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Context
+{
+    public class BackendContext : IdentityDbContext<User>
+    {
+        public BackendContext(DbContextOptions<BackendContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().ToTable("Users");
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("CreatedAt") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("CreatedAt").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("CreatedAt").IsModified = false;
+                }
+            }
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("UpdatedAt") != null))
+            {
+                if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+                {
+                    entry.Property("UpdatedAt").CurrentValue = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
---
+++ src/Domain/Entity/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        //public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        //{
+        //    var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        //    return userIdentity;
+        //}
+    }
+}
